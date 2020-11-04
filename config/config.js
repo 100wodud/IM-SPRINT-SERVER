0b1e7bd621e1614e5494303719274e8053f8e1a4
100wodud <58911000+100wodud@users.noreply.github.com>
2020-11-04 11:00:43 +0900

    Update config.js

--- config/config.js
+++ config/config.js
@@ -5,22 +5,25 @@ module.exports = {
     "username": process.env.DATABASE_USERNAME,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
-    "host": "localhost",
-    "dialect": "mysql"
+    "host": "database-2.ca8pdbmenue7.ap-northeast-2.rds.amazonaws.com",
+    "dialect": "mysql",
+    "port: "13302"
   },
   "test": {
     "username": process.env.DATABASE_USERNAME,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
-    "host": "localhost",
-    "dialect": "mysql"
+    "host": "database-2.ca8pdbmenue7.ap-northeast-2.rds.amazonaws.com",
+    "dialect": "mysql",
+    "port: "13302"
   },
   "production": {
     "username": process.env.DATABASE_USERNAME,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
-    "host": "localhost",
-    "dialect": "mysql"
+    "host": "database-2.ca8pdbmenue7.ap-northeast-2.rds.amazonaws.com",
+    "dialect": "mysql",
+    "port: "13302"
   }
 }
 
