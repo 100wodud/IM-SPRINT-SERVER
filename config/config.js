c5f3c01fcd4c8d63f8c949bd4a2fd76a8e04b775
100wodud <58911000+100wodud@users.noreply.github.com>
2020-11-04 11:11:33 +0900

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
   "development": {
     "username": process.env.DATABASE_USERNAME,
-    "password": process.env.DATABASE_PASSWORD,
+    "password": "kin454536",
     "database": process.env.DATABASE_NAME,
     "host": "database-2.ca8pdbmenue7.ap-northeast-2.rds.amazonaws.com",
     "dialect": "mysql",
